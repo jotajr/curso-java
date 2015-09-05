39ca6d29fbae8b71780e7a5fe1dec88970e25988
Jota Freitas Jr <jotajr@jotajr.com>
2015-09-05 09:02:36 -0300

    Adicionado o capítulo 2 (incompleto)

---
+++ codigo-fonte/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+public class PrimeiroPrograma {
+
+   /* Meu Primeiro programa em Java  
+    * O velho Hello World que imprime Ola Mundo
+    * na saida do console
+    */
+    
+    public static void main(String []args) {
+       System.out.println("Ola Mundo"); // imprime Ola Mundo
+    }
+}
